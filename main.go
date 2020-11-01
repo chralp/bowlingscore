26e3699a746752323d629e7cbf2d094f3b8a510d
Christian Lind Alpstad <christian@alpstad.se>
2020-11-01 23:11:51 +0100

    update

--- main.go
+++ main.go
@@ -2,30 +2,28 @@ package main
 
 import (
 	"./bowling"
+	"fmt"
 )
-
 
 func main(){
 
 	// Start bowling game
 	bowling.Startgame(1)
-
+	
 	// Append rolls
 	bowling.GetCurrentGame("player1").AddRoll("x")
-	bowling.GetCurrentGame("player1").AddRoll("9")
-	bowling.GetCurrentGame("player1").AddRoll("-")
-	bowling.GetCurrentGame("player1").AddRoll("7")
-	bowling.GetCurrentGame("player1").AddRoll("/")
 	bowling.GetCurrentGame("player1").AddRoll("x")
 	bowling.GetCurrentGame("player1").AddRoll("x")
 	bowling.GetCurrentGame("player1").AddRoll("x")
-	bowling.GetCurrentGame("player1").AddRoll("5")
-	bowling.GetCurrentGame("player1").AddRoll("-")
-	bowling.GetCurrentGame("player1").AddRoll("8")
-	bowling.GetCurrentGame("player1").AddRoll("1")
+	bowling.GetCurrentGame("player1").AddRoll("x")
+	bowling.GetCurrentGame("player1").AddRoll("x")
+	bowling.GetCurrentGame("player1").AddRoll("x")
+	bowling.GetCurrentGame("player1").AddRoll("x")
 	bowling.GetCurrentGame("player1").AddRoll("x")
 	bowling.GetCurrentGame("player1").AddRoll("x")
 	bowling.GetCurrentGame("player1").AddRoll("x")
 	bowling.GetCurrentGame("player1").AddRoll("x")
 	
+	Game := bowling.GetCurrentGame("player1")
+	fmt.Println(Game) // 300
 }
